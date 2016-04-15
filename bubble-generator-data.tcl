418a4d365e389c6931616ad1499dd29335c140d1
Anton Yabchinskiy <arn@bestmx.net>
2016-04-15 11:23:48 +0300

    Initial import

---
+++ bubble-generator-data.tcl
@@ -0,0 +1,13 @@
+# This file contains the data used by the three syntax diagram rendering
+# programs:
+#
+#   bubble-generator.tcl
+#   bubble-generator-bnf.tcl
+#   bubble-generator-bnf.tcl
+#
+
+# Graphs:
+#
+set all_graphs {
+
+}
